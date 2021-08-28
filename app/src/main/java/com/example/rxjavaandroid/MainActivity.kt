068ce1707b6a41a00c934cfa557730ab4cdc2dc4
Momin Tahir <momintahir7@gmail.com>
2021-08-29 01:39:14 +0500

    Working with Observable just method

--- app/src/main/java/com/example/rxjavaandroid/MainActivity.kt
+++ app/src/main/java/com/example/rxjavaandroid/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.rxjavaandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.reactivex.Observable
+import io.reactivex.Observer
+import io.reactivex.disposables.Disposable
+import io.reactivex.functions.Consumer
+import java.lang.RuntimeException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Observable.just("apple", "orange", "banana")
+            .map({
+                throw RuntimeException()
+            })
+            .subscribe(
+                object : Observer<String> {
+                    override fun onSubscribe(d: Disposable) {
+                        println("onSubsribe")
+                    }
+
+                    override fun onNext(t: String) {
+                        println(t)
+                    }
+
+                    override fun onError(e: Throwable) {
+                        println("on error $e")
+                    }
+
+                    override fun onComplete() {
+                        println("oncomplete")
+                    }
+
+                }
+            )
+
     }
 }
